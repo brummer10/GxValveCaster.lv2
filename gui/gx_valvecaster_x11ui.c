5f039d96f898972050ef78a620cef5c4c9498240
Thorsten Maerz <info@netztorte.de>
2023-01-12 19:29:18 +0100

    remove old gtk workaround (reset_request())

--- gui/gx_valvecaster_x11ui.c
+++ gui/gx_valvecaster_x11ui.c
@@ -652,12 +652,6 @@ static void controller_expose(const gx_valvecasterUI * const ui,  const gx_contr
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-// hack to allow resizing below the initial size on gtk based hosts
-static void reset_request(gx_valvecasterUI * const ui) {
-	ui->resize->ui_resize(ui->resize->handle, ui->width/1.4, ui->height/1.4);
-	ui->first_resize = false;
-}
-
 // resize the xwindow and the cairo xlib surface
 static void resize_event(gx_valvecasterUI * const ui) {
 	XWindowAttributes attrs;
@@ -979,7 +973,6 @@ static void event_handler(gx_valvecasterUI * const ui) {
 					break;
 					case Button2:
 						// click on the mouse wheel
-						if (ui->first_resize) reset_request(ui);
 						//debug_print("Button2 \n");
 					break;
 					case Button3:
